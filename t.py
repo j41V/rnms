bb4ae8b1492e235d935f0b374a5abdc1c8ea1872
jaiv <jaiv@jaiv.com>
2024-11-24 23:59:13 +0100

    cleaned project

--- t.py
+++
@@ -1,6 +0,0 @@
-port_file = open("tcp.txt", "r").readlines()
-write_file = open("test", "w")
-for line in port_file:
-    desc, port = line.strip().strip("/udp").split(",")
-    write_string = port + " => " + '"' + desc + '".to_string(),\n'
-    write_file.write(write_string)
